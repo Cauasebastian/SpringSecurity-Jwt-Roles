3c50d402aae5fd457f7f7c57486ad4bb84d44016
Cauã Sebastian <133614515+Cauasebastian@users.noreply.github.com>
2024-07-18 01:24:32 -0300

    SecurityConfigurations
    Application being Stateless

---
+++ src/main/java/org/sebastiandev/springsecurityjwtroles/services/AuthorizationService.java
@@ -0,0 +1,20 @@
+package org.sebastiandev.springsecurityjwtroles.services;
+
+import org.sebastiandev.springsecurityjwtroles.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AuthorizationService  implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public AuthorizationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return userRepository.findByLogin(login);
+    }
+}
