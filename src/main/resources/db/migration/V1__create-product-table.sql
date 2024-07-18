c395548a1743c45bd3f235dca46559f3dc2d4cbf
Laranunes1 <133614515+Cauasebastian@users.noreply.github.com>
2024-07-17 22:58:22 -0300

    initial commit

---
+++ src/main/resources/db/migration/V1__create-product-table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE product (
+                         id VARCHAR(255) PRIMARY KEY,
+                         name TEXT NOT NULL,
+                         price INTEGER NOT NULL
+);
---
+++ src/test/java/org/sebastiandev/springsecurityjwtroles/SpringSecurityJwtRolesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sebastiandev.springsecurityjwtroles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtRolesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
